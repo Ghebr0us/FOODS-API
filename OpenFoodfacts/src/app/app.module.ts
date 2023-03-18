27bc3006aedf84bcb2eb624f2828098d0e9ecc2b
Ghebr0us <ghebrous.davide@iisgalvanimi.edu.it>
2023-03-18 17:53:45 +0000

    punto 4

--- OpenFoodfacts/src/app/app.module.ts
+++ OpenFoodfacts/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { DescriptionComponent } from './description/description.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// npm i @ng-bootstrap/ng-bootstrap
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { DescriptionComponent } from './description/description.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- OpenFoodfacts/src/app/search/search.component.ts
+++ OpenFoodfacts/src/app/search/search.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/services/api.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  data!: any;
 
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  search(e: any) {
+    var q = e.target.value;
+    this.api.products(q).subscribe((data: any) => this.data = data);
+    console.log(this.data)
+  }
 }
